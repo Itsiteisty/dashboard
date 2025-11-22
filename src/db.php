cfeebb44bfc9982e595e82dd21caa2418bfda488
qssseSammyinks <gayarter8@gmail.com>
2025-11-22 18:23:56 +0000

    rende

--- src/db.php
+++ src/db.php
@@ -1,8 +1,11 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/auth.php';
 
 $mongoUri = getenv('MONGO_URI') ?: 'mongodb://localhost:27017';
 $client = new MongoDB\Client($mongoUri);
+
 $db = $client->selectDatabase('dashusers');
 $collection = $db->users;
-?>
+
+var_dump($collection->countDocuments()); // teste rápido
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c884e87d998be1b42547f0568a55c4bce1c7b670',
+        'reference' => 'e03ed53fb1187510635793da296926bd8c5d7666',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c884e87d998be1b42547f0568a55c4bce1c7b670',
+            'reference' => 'e03ed53fb1187510635793da296926bd8c5d7666',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
